a420ef285e694b2c23c4fe79afc94ad463a9a8b2
Julia Nguyen <julianguyenpt@gmail.com>
2026-03-20 14:54:13 -0400

    feat: random forest and lasso

--- workflow/notebooks/utils/helpers.py
+++ workflow/notebooks/utils/helpers.py
@@ -48,10 +48,99 @@ def run_shap(X, final_model, filepath):
     plt.close()
 
 
+"""
+Simple ML models with nested 5foldCV
+    * LASSO
+    * Elastic Net
+    * Random Forest
+"""
+
+def run_lasso(X, y, path):
+
+    # initialize outer folds (5 folds, 80% train, 20% test)
+    outer_cv = KFold(n_splits=5, shuffle=True, random_state=101)
+
+    # initialize variables to store results
+    results = []
+    hyperparams_list = []
+    fold = 1
+
+    # loop through each of the outer five folds
+    for train_index, test_index in outer_cv.split(X):
+
+        # initialize inner folds (5 folds, 80% train, 20% test)
+        inner_cv = KFold(n_splits=5, shuffle=True, random_state=fold)
+
+        # split train and test
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        # initialize LASSO model
+        lasso = linear_model.Lasso()
+
+        # specify parameters for optimization
+        parameters = {
+            'alpha': [0.001, 0.01, 0.1, 1, 10, 100],
+            'max_iter': [500, 1000, 5000, 7500]
+        }
+
+        # identify optimal parameters
+        reg = GridSearchCV(
+            estimator = lasso,
+            param_grid = parameters,
+            cv=inner_cv,
+            scoring=make_scorer(spearman_scorer, greater_is_better=True),
+            n_jobs=-1
+        )
+
+        # fit model
+        reg.fit(X_train, y_train)
+
+        # get best model & parameters
+        reg_best = reg.best_estimator_
+
+        best_params = reg.best_params_
+        hyperparams_list.append(best_params)
+
+        # get predicted values for test data
+        y_pred = reg_best.predict(X_test)
+
+        # compute metrics
+        s_corr = spearmanr(y_test, y_pred).correlation
+        p_corr = pearsonr(y_test, y_pred)[0]
+        rmse = root_mean_squared_error(y_test, y_pred)
+        mae = mean_absolute_error(y_test, y_pred)
+
+        # save model correlation and features
+        results.append({
+            "fold": fold,
+            "spearman": s_corr,
+            "pearson": p_corr,
+            "rmse": rmse,
+            "mae": mae,
+            **best_params
+        })
+
+        fold += 1
+
+    # save results to dataframe
+    results_df = pd.DataFrame(results)
+    results_df.to_csv(("data/results/data/"+path+"lasso_folds.csv"), index=False)
+
+    # get most common hyperparameters
+    #params_df = pd.DataFrame(hyperparams_list)
+    #final_params = params_df.mode().iloc[0].to_dict()
+    #final_params["max_iter"] = int(final_params["max_iter"])
+
+    # fit final model
+    #final_model = linear_model.Lasso(**final_params)
+    #final_model.fit(X, y)
+
+    # run shap
+    #run_shap(X, final_model, path)
+
+
 def run_elastic_net(X, y, path):
-    """
-    Run Elastic Net with nested 5foldCV
-    """
 
     # initialize outer folds (5 folds, 80% train, 20% test)
     outer_cv = KFold(n_splits=5, shuffle=True, random_state=101)
@@ -115,9 +204,7 @@ def run_elastic_net(X, y, path):
             "pearson": p_corr,
             "rmse": rmse,
             "mae": mae,
-            "alpha": best_params["alpha"],
-            "l1_ratio": best_params["l1_ratio"],
-            "max_iter": best_params["max_iter"]
+            **best_params
         })
 
         fold += 1
@@ -127,16 +214,100 @@ def run_elastic_net(X, y, path):
     results_df.to_csv(("data/results/data/"+path+"en_folds.csv"), index=False)
 
     # get most common hyperparameters
-    params_df = pd.DataFrame(hyperparams_list)
-    final_params = params_df.mode().iloc[0].to_dict()
+    #params_df = pd.DataFrame(hyperparams_list)
+    #final_params = params_df.mode().iloc[0].to_dict()
 
     # fit final model
-    final_model = linear_model.ElasticNet(
-        alpha=final_params["alpha"],
-        l1_ratio=final_params["l1_ratio"],
-        max_iter=int(final_params["max_iter"])
-    )
-    final_model.fit(X, y)
+    #final_model = linear_model.ElasticNet(**final_params)
+    #final_model.fit(X, y)
+
+    # run shap
+    #run_shap(X, final_model, path)
+
+def run_random_forest(X, y, path):
+
+    # initialize outer folds (5 folds, 80% train, 20% test)
+    outer_cv = KFold(n_splits=5, shuffle=True, random_state=101)
+
+    # initialize variables to store results
+    results = []
+    hyperparams_list = []
+    fold = 1
+
+    # loop through each of the outer five folds
+    for train_index, test_index in outer_cv.split(X):
+
+        # initialize inner folds (5 folds, 80% train, 20% test)
+        inner_cv = KFold(n_splits=5, shuffle=True, random_state=fold)
+
+        # split train and test
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        # initialize random forest model
+        rf = RandomForestRegressor()
+
+        # specify parameters for optimization
+        parameters = {
+            'n_estimators': [10, 50, 100, 150, 200],
+            'max_depth': [None, 10, 20],
+            'min_samples_split': [2, 5],
+            'min_samples_leaf': [1, 2, 5],
+            'max_features': ['sqrt', 'log2']
+        }
+
+        # identify optimal parameters
+        reg = GridSearchCV(
+            estimator = rf,
+            param_grid = parameters,
+            cv=inner_cv,
+            scoring=make_scorer(spearman_scorer, greater_is_better=True),
+            n_jobs=-1
+        )
+
+        # fit model
+        reg.fit(X_train, y_train)
+
+        # get best model & parameters
+        reg_best = reg.best_estimator_
+
+        best_params = reg.best_params_
+        hyperparams_list.append(best_params)
+
+        # get predicted values for test data
+        y_pred = reg_best.predict(X_test)
+
+        # compute metrics
+        s_corr = spearmanr(y_test, y_pred).correlation
+        p_corr = pearsonr(y_test, y_pred)[0]
+        rmse = root_mean_squared_error(y_test, y_pred)
+        mae = mean_absolute_error(y_test, y_pred)
+
+        # save model correlation and features
+        results.append({
+            "fold": fold,
+            "spearman": s_corr,
+            "pearson": p_corr,
+            "rmse": rmse,
+            "mae": mae,
+            **best_params
+        })
+
+        fold += 1
+
+    # save results to dataframe
+    results_df = pd.DataFrame(results)
+    results_df.to_csv(("data/results/data/"+path+"rf_folds.csv"), index=False)
+
+    # get most common hyperparameters
+    #params_df = pd.DataFrame(hyperparams_list)
+    #final_params = params_df.mode().iloc[0].to_dict()
+    #final_params["max_depth"] = int(final_params["max_depth"])
+    #final_params["min_samples_leaf"] = int(final_params["min_samples_leaf"])
+
+    # fit final model
+    #final_model = RandomForestRegressor(**final_params)
+    #final_model.fit(X, y)
 
     # run shap
     #run_shap(X, final_model, path)

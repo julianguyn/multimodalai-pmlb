db24f4b60c78a4ab145e95ee9e3d0b1cc962aee8
Julia Nguyen <julianguyenpt@gmail.com>
2026-03-31 17:12:19 -0400

    feat: lasso select common genes

---
+++ sandbox/sbatch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=lasso
+#SBATCH --output=logs/log.out
+#SBATCH --error=logs/log.err
+#SBATCH --time=02:00:00
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=2
+
+conda init --all
+conda activate /cluster/home/julian/miniconda3/envs/pmlb
+
+INPUT_NOTEBOOK="notebooks/6-common_genes_models.ipynb"
+OUTPUT_NOTEBOOK="notebooks/6-common_genes_models_exc.ipynb"
+
+jupyter nbconvert --to notebook \
+                  --execute "$INPUT_NOTEBOOK" \
+                  --output "$OUTPUT_NOTEBOOK" \
+                  --ExecutePreprocessor.timeout=-1
+
+echo "Done, saved to $OUTPUT_NOTEBOOK"
--- workflow/notebooks/utils/early_helpers.py
+++ workflow/notebooks/utils/early_helpers.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import KFold, GridSearchCV
 from sklearn import linear_model
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error, mean_absolute_error, make_scorer
-import xgboost as xgb
 from scipy.stats import spearmanr, pearsonr
-import shap
+from collections import Counter
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 newcmp = mcolors.LinearSegmentedColormap.from_list("custom", ["blue", "red"])
@@ -55,78 +54,90 @@ Simple ML models with nested 5foldCV
     * Random Forest
 """
 
-def run_lasso(X, y, path, preds=False):
-
-    # initialize outer folds (5 folds, 80% train, 20% test)
-    outer_cv = KFold(n_splits=5, shuffle=True, random_state=101)
+def run_lasso(X, y, path, repeats=10, preds=False, features=False):
 
     # initialize variables to store results
     fold_preds = pd.Series(index=y.index, dtype=float)
     results = []
     hyperparams_list = []
-    fold = 1
+    feature_counter = Counter()
+    total_models = 0
 
-    # loop through each of the outer five folds
-    for train_index, test_index in outer_cv.split(X):
+    # repeats of 5foldCV
+    for repeat in range(repeats):
 
-        # initialize inner folds (5 folds, 80% train, 20% test)
-        inner_cv = KFold(n_splits=5, shuffle=True, random_state=fold)
+        # initialize outer folds (5 folds, 80% train, 20% test)
+        outer_cv = KFold(n_splits=5, shuffle=True, random_state=101 + repeat)
+        fold = 1
 
-        # split train and test
-        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        # loop through each of the outer five folds
+        for train_index, test_index in outer_cv.split(X):
 
-        # initialize LASSO model
-        lasso = linear_model.Lasso()
+            # initialize inner folds (5 folds, 80% train, 20% test)
+            inner_cv = KFold(n_splits=5, shuffle=True, random_state=fold)
 
-        # specify parameters for optimization
-        parameters = {
-            'alpha': [0.001, 0.01, 0.1, 1, 10, 100],
-            'max_iter': [500, 1000, 5000, 7500]
-        }
+            # split train and test
+            X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-        # identify optimal parameters
-        reg = GridSearchCV(
-            estimator = lasso,
-            param_grid = parameters,
-            cv=inner_cv,
-            scoring=make_scorer(spearman_scorer, greater_is_better=True),
-            n_jobs=-1
-        )
+            # initialize LASSO model
+            lasso = linear_model.Lasso()
 
-        # fit model
-        reg.fit(X_train, y_train)
+            # specify parameters for optimization
+            parameters = {
+                'alpha': [0.001, 0.01, 0.1, 1, 10, 100],
+                'max_iter': [500, 1000, 5000, 7500]
+            }
 
-        # get best model & parameters
-        reg_best = reg.best_estimator_
+            # identify optimal parameters
+            reg = GridSearchCV(
+                estimator = lasso,
+                param_grid = parameters,
+                cv=inner_cv,
+                scoring=make_scorer(spearman_scorer, greater_is_better=True),
+                n_jobs=-1
+            )
 
-        best_params = reg.best_params_
-        hyperparams_list.append(best_params)
+            # fit model
+            reg.fit(X_train, y_train)
 
-        # get predicted values for test data
-        y_pred = reg_best.predict(X_test)
+            # get best model & parameters
+            reg_best = reg.best_estimator_
 
-        # compute metrics
-        s_corr = spearmanr(y_test, y_pred).correlation
-        p_corr = pearsonr(y_test, y_pred)[0]
-        rmse = root_mean_squared_error(y_test, y_pred)
-        mae = mean_absolute_error(y_test, y_pred)
+            best_params = reg.best_params_
+            hyperparams_list.append(best_params)
 
-        # save model correlation and features
-        results.append({
-            "fold": fold,
-            "spearman": s_corr,
-            "pearson": p_corr,
-            "rmse": rmse,
-            "mae": mae,
-            **best_params
-        })
+            # get selected features
+            selected_features = X.columns[reg_best.coef_ != 0]
+            feature_counter.update(selected_features)
+            total_models += 1
 
-        fold += 1
+            # get predicted values for test data
+            y_pred = pd.Series(reg_best.predict(X_test), index=y_test.index)
+            fold_preds.loc[y_test.index] = y_pred
+
+            # compute metrics
+            s_corr = spearmanr(y_test, y_pred).correlation
+            p_corr = pearsonr(y_test, y_pred)[0]
+            rmse = root_mean_squared_error(y_test, y_pred)
+            mae = mean_absolute_error(y_test, y_pred)
+
+            # save model correlation and features
+            results.append({
+                "repeat": repeat+1,
+                "fold": fold,
+                "spearman": s_corr,
+                "pearson": p_corr,
+                "rmse": rmse,
+                "mae": mae,
+                **best_params
+            })
+
+            fold += 1
 
     # save results to dataframe
     results_df = pd.DataFrame(results)
-    results_df.to_csv(("data/results/data/"+path+"lasso_folds.csv"), index=False)
+    results_df.to_csv(f"data/results/data/{path}lasso_folds.csv", index=False)
 
     # get most common hyperparameters
     #params_df = pd.DataFrame(hyperparams_list)
@@ -140,6 +151,17 @@ def run_lasso(X, y, path, preds=False):
     # run shap
     #run_shap(X, final_model, path)
 
+    if features:
+        # save features to dataframe
+        feature_freq = pd.DataFrame.from_dict(feature_counter, orient='index', columns=['count'])
+        feature_freq['frequency'] = feature_freq['count'] / total_models
+        feature_freq = feature_freq.sort_values(by='frequency', ascending=False)
+        feature_freq.to_csv(f"data/results/data/{path}lasso_feature_stability.csv")
+        #stable_features = feature_freq[feature_freq['frequency'] >= 0.6].index.tolist()
+        return feature_freq
+
+    if preds:
+        return fold_preds
 
 def run_elastic_net(X, y, path, preds=False):
 
@@ -148,14 +170,6 @@ def run_elastic_net(X, y, path, preds=False):
 
     # initialize variables to store results
     fold_preds = pd.Series(index=y.index, dtype=float)
-    results = []
-    hyperparams_list = []
-    fold = 1
-
-    # initialize outer folds (5 folds, 80% train, 20% test)
-    outer_cv = KFold(n_splits=5, shuffle=True, random_state=101)
-
-    # initialize variables to store results
     results = []
     hyperparams_list = []
     fold = 1
@@ -236,6 +250,7 @@ def run_elastic_net(X, y, path, preds=False):
     #run_shap(X, final_model, path)
 
     if preds:
+        feature_weights = pd.Series(final_model.coef_, index=X.columns)
         return fold_preds
 
 def run_random_forest(X, y, path):

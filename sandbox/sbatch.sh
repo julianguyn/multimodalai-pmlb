76feffe703f90e09f19773b7698dd1afcdb6a4f8
Julia Nguyen <julianguyenpt@gmail.com>
2026-03-31 23:16:43 -0400

    feat: normalize omics matrices

--- sandbox/sbatch.sh
+++ sandbox/sbatch.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=2
 
 conda init --all
-conda activate /cluster/home/julian/miniconda3/envs/pmlb
+conda activate /cluster/home/julian/.conda/envs/pmlb
 
 INPUT_NOTEBOOK="notebooks/6-common_genes_models.ipynb"
 OUTPUT_NOTEBOOK="notebooks/6-common_genes_models_exc.ipynb"

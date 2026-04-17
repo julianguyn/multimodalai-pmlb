a271c9c544e1e623bf232999f5d28abab099b310
Julia Nguyen <julianguyenpt@gmail.com>
2026-04-17 19:36:30 -0400

    fix: fixes for ben

--- workflow/scripts/temp-plots-for-James.R
+++ workflow/scripts/temp-plots-for-James.R
@@ -20,17 +20,97 @@ toPlot <- toPlot %>%
 
 toPlot[upper.tri(toPlot)] <- NA
 
+# get rid of diagonal
+for (i in 1:5) {
+  toPlot[i,i] <- NA
+}
+
+
 Heatmap(
   toPlot,
   name = "Correlation",
   na_col = "white",
-  col = colorRamp2(c(0.7, 0.85, 1), c("#66c2a5", "#fc8d62", "#8da0cb")),
+  col = colorRamp2(c(0.75, 0.85, 1), c("#66c2a5", "#fc8d62", "#8da0cb")),
   cluster_rows = FALSE,
   cluster_columns = FALSE,
-  cell_fun = function(j, i, x, y, width, height, fill) {
-    val <- toPlot[i, j]
-    if (!is.na(val)) {
-        grid.text(sprintf("%.2f", val), x, y, gp = gpar(fontsize = 8))
-    }
-    }
+)
+
+# color option 1: blue white pueple
+Heatmap(
+  toPlot,
+  name = "Correlation",
+  na_col = "white",
+  col = colorRamp2(c(0.75, 0.85, 1), c("#7F7CAF", "#9FB4C7", "#28587B")),
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+)
+
+# color option 2: red orange yellow
+Heatmap(
+  toPlot,
+  name = "Correlation",
+  na_col = "white",
+  col = colorRamp2(c(0.75, 0.85, 1), c("#F7B538", "#DB7C26", "#C32F27")),
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+)
+
+# color option 3: orange and browns
+Heatmap(
+  toPlot,
+  name = "Correlation",
+  na_col = "white",
+  col = colorRamp2(c(0.75, 0.85, 1), c("#A57F60", "#E8AE68", "#DB5A42")),
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+)
+
+# color option 4: pink and blue
+Heatmap(
+  toPlot,
+  name = "Correlation",
+  na_col = "white",
+  col = colorRamp2(c(0.75, 0.85, 1), c("#93BFB2", "#C5E0D8", "#CEABB1")),
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+)
+
+# color option 5: teal and taupe
+Heatmap(
+  toPlot,
+  name = "Correlation",
+  na_col = "white",
+  col = colorRamp2(c(0.75, 0.85, 1), c("#A18276", "#FCDFA6", "#AAC0AA")),
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+)
+
+# color option 5: teal and taupe again ish
+Heatmap(
+  toPlot,
+  name = "Correlation",
+  na_col = "white",
+  col = colorRamp2(c(0.75, 0.85, 1), c("#6D5959", "#9DCBBA", "#ABEBD2")),
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+)
+
+# color option 6: YELLOW blue
+Heatmap(
+  toPlot,
+  name = "Correlation",
+  na_col = "white",
+  col = colorRamp2(c(0.75, 0.85, 1), c("#294C60", "#ADB6C4", "#FFC49B")),
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+)
+
+# color option 6: YELLOW blue
+Heatmap(
+  toPlot,
+  name = "Correlation",
+  na_col = "white",
+  col = colorRamp2(c(0.75, 0.85, 1), c("#565676", "#AEADF0", "#A76571")),
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
 )
